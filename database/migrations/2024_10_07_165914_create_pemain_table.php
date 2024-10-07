36ed538aa6d198aa3703966e0f213d1d9219f192
Amelia2806 <ameliaayunitricahyanti2005@mail.ugm.ac.id>
2024-10-08 00:40:29 +0700

    first commit

---
+++ database/migrations/2024_10_07_165914_create_pemain_table.php
@@ -0,0 +1,23 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePemainTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('pemain', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_pemain');
+            $table->integer('no_punggung');
+            $table->string('posisi');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('pemain');
+    }
+}
---
+++ resources/views/pemain/index.blade.php
@@ -0,0 +1,28 @@
+{{-- resources/views/pemain/index.blade.php --}}
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1>Pemain</h1>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nama Pemain</th>
+                    <th>No Punggung</th>
+                    <th>Posisi</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($pemain as $player)
+                    <tr>
+                        <td>{{ $player->id }}</td>
+                        <td>{{ $player->nama_pemain }}</td>
+                        <td>{{ $player->no_punggung }}</td>
+                        <td>{{ $player->posisi }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+@endsection
